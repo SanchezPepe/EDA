2b5d3f21c18f2ca92a3b38a0e83579cceccdffbb
SanchezPepe <sanchezpepe97@gmail.com>
2016-10-24 00:54:00 -0500

    Reparación de métodos

--- src/SkipList/SkipList.java
+++ src/SkipList/SkipList.java
@@ -12,18 +12,20 @@ import java.util.Random;
  * @author hca
  */
 public class SkipList<T extends Comparable<T>>{
-    
+
     private int cont;
     private NodoS<T> cabeza, cola;
     private Random r;
-    
+    private int altura;
+
     public SkipList(){
         cont = 0;
+        altura = 0;
         cabeza.setRigth(cola);
         cola.setLeft(cabeza);
         r = new Random();
     }
-    
+
     public SkipList(T elem){
         this();
         if(elem != null){
@@ -35,20 +37,20 @@ public class SkipList<T extends Comparable<T>>{
             cola.setLeft(nuevo);
         }
     }
-    
+
     public boolean coinFlip(){
         return r.nextBoolean();
     }
-        
+
     public NodoS<T> find(T elem){
         NodoS aux = cabeza.getRigth();
         return find(aux, elem);
     }
-    
+
     private NodoS<T> find(NodoS<T> aux, T elem){
         if(aux.getElem().equals(elem)){ //SI LO ENCONTRÉ
             if(aux.getDown() == null) //SI NO TIENE ABAJO
-                return aux;                     
+                return aux;
             else{
                 while(aux.getDown() != null) //SI TIENE ABAJO
                     aux = aux.getDown();
@@ -59,13 +61,13 @@ public class SkipList<T extends Comparable<T>>{
             if(elem.compareTo(aux.getElem()) > 0 && aux.getRigth() != null) //SI ELEMENTO ES MAYOR AL NODO Y TIENE DERECHA - SE RECORRE
                 return find(aux.getRigth(), elem);
             if(aux.getDown() != null) //SI TIENE ABAJO - SE RECORRE ABAJO
-                return find(aux.getDown(), elem); 
+                return find(aux.getDown(), elem);
             else //SI ELEMENTO ES MENOR AL NODO Y NO TIENE ABAJO = NO LO ENCONTRÓ - REGRESA DONDE DEBERÍA IR
                 return aux.getLeft();
-            
+
         }
     }
-    
+
     //ACOMODAR APUNTADORES EN HORIZONAL
     private void ligaH(NodoS<T> izq, NodoS<T> nuevo, NodoS<T> der){
         izq.setRigth(nuevo);
@@ -73,7 +75,7 @@ public class SkipList<T extends Comparable<T>>{
         nuevo.setRigth(der);
         der.setLeft(nuevo);
     }
-    
+
     //ACOMODAR APUNTADORES EN VERTICAL
     private void ligaV(NodoS<T> arriba, NodoS<T> nuevo, NodoS<T> abajo){
         arriba.setDown(nuevo);
@@ -81,33 +83,16 @@ public class SkipList<T extends Comparable<T>>{
         nuevo.setDown(abajo);
         abajo.setUp(nuevo);
     }
-    
-    private void nuevoNivel(NodoS<T> nuevo){
+
+    //CREACION DE UN NUEVO NIVEL SI ARRIBA ES NULO y FLIP COIN == TRUE
+    private void nuevoNivel(NodoS<T> nodo){
+    }
+
+
+    public void insert(T elem){
         
     }
-    
-    public void insert(T elem){
-        NodoS<T> nuevo = new NodoS(elem);;
-        NodoS<T> ant = find(elem);
-        NodoS<T> aux = cabeza;
-        int niveles = 0;
-        if(ant == null){ //SKIPLIST VACÍA
-            ligaH(cabeza, nuevo, cola);
-            //FLIP-COIN
-        } else{ //ENCONTRÓ EL ELEMENTO ANTERIOR
-            ligaH(ant, nuevo, ant.getRigth());
-            cont++;
-        }
-        //FLIP COIN
-        while(aux.getDown() != null)
-            niveles++;
-        Random r = new Random();
-        if(r.nextBoolean()){
-            
-        }
-        //ELSE, TERMINÓ DE INSERTAR
-    }
-    
+
     public static void main(String[] args) {
         Random r = new Random();
         System.out.println(r.nextBoolean());
