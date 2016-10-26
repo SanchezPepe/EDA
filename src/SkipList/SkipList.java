8f9522b001d95d0ad5fb2cb48b5d315602f5c4a0
unknown <jsanchezagu@CC319.itam.mx>
2016-10-26 15:24:25 -0500

    AHORA SI JALA COMPA

--- src/SkipList/SkipList.java
+++ src/SkipList/SkipList.java
@@ -71,35 +71,51 @@ public class SkipList<T extends Comparable<T>>{
         }
     }
     
-    public NodoS<T> buscaLista(NodoS<T> nodo, T elem){
-        if(esCola(nodo))
-            return nodo.getLeft();
-        if(elem.compareTo(nodo.getElem()) > 0)
-            return buscaLista(nodo.getRigth(), elem);
-        else
-            return nodo.getLeft();
-    }
-    
     public void insertN(T elem){
         NodoS<T> ant, nuevo, ai, ad;
         if(elem != null){
             nuevo = new NodoS(elem);
             ant = find(elem);
+            //System.out.println("IZQUIERDA: " + ant.getElem() + "\nCENTRO: " + nuevo.getElem() + "\nDERECHA: " + ant.getRigth().getElem());
             this.ligaH(ant, nuevo , ant.getRigth());
             cont++;
-            while(flipCoin() && altura < Math.log(cont)){
+            boolean res = flipCoin();
+            //System.out.println(res);
+            while(res && altura < Math.log(cont)){
                 ai = findUpL(nuevo);
                 ad = findUpR(nuevo);
+                //System.out.println("ENTRO NUEVA LINEA CON: " + ai.getElem()+ " " + ad.getElem());
                 this.ligaH(ai, cloneUp(nuevo), ad);
+                //System.out.println("NUEVA LIGA: " + ai.getElem()+ " " + ai.getRigth().getElem()+ " " + ad.getElem());
                 nuevo = nuevo.getUp();
-                altura++;
-                
+                res = flipCoin();
             }
         }
     }
     
+    public void borra(T elem){
+        NodoS<T> busq = this.find(elem);
+        if(busq.getElem().equals(elem)){
+            if(busq.getUp() == null)
+                desliga(busq);
+            while(busq.getUp() != null){
+                desliga(busq);
+                busq = busq.getUp();
+            }
+        }else
+            System.out.println("El elemento: '" + elem + "' no está en la estrucura");
+    }
+    
     //MÉTODOS AUXILIARES
-    //VOLADO
+    public void desliga(NodoS<T> nodo){
+        NodoS<T> izq = nodo.getLeft();
+        NodoS<T> der = nodo.getRigth();
+        izq.setRigth(der);
+        der.setLeft(izq);
+        nodo.setLeft(null);
+        nodo.setRigth(null);
+    }
+    
     public NodoS<T> findUpR(NodoS<T> nodo){
         NodoS<T> aux = nodo;
         while(aux.getRigth() != null){
@@ -124,27 +140,25 @@ public class SkipList<T extends Comparable<T>>{
         NodoS<T> up = new NodoS(nodo.getElem());
         nodo.setUp(up);
         up.setDown(nodo);
-        if(nodo == cabeza)
+        if(nodo == cabeza){
             cabeza = up;
+            altura++;
+        }
+            
         if(nodo == cola)
             cola = up;
         return up;
     }
     
-    
-    
     public boolean flipCoin(){
         return r.nextBoolean();
-    }
-    
-    public boolean esCabeza(NodoS<T> nodo){
-        return nodo.getLeft() == null && nodo.getElem() == null;
     }
     
     public boolean esCola(NodoS<T> nodo){
         return nodo.getRigth() == null && nodo.getElem() == null;
     }
-    //ACOMODAR APUNTADORES EN HORIZONAL
+    
+//ACOMODAR APUNTADORES EN HORIZONAL
     private void ligaH(NodoS<T> izq, NodoS<T> nuevo, NodoS<T> der){
         izq.setRigth(nuevo);
         nuevo.setLeft(izq);
@@ -169,14 +183,18 @@ public class SkipList<T extends Comparable<T>>{
         StringBuilder cad = new StringBuilder();
         NodoS<T> aux = cabeza;
         NodoS<T> aux2 = cabeza;
+        while(aux.getDown() != null){
+            aux = aux.getDown();
+            aux2 = aux2.getDown();
+        }
         for(int i = 0; i < ancho; i++){
             for(int j = 0; j <= altura; j++){
-                cad.append(aux.getElem()).append("   ");
+                if(aux.getElem() == null)
+                    cad.append("(" + aux.getElem() + ")").append("---->");
+                else
+                    cad.append("( " + aux.getElem() + "  )").append("--->");
                 if(aux.getUp() == null)
-                    while(j <= altura){
-                        cad.append(" ");
-                        j++;
-                    }
+                    break;
                 else
                     aux = aux.getUp();
             }
@@ -191,10 +209,16 @@ public class SkipList<T extends Comparable<T>>{
     
     public static void main(String[] args) {
         SkipList<Integer> l = new SkipList();
-        for(int i = 0; i < 4; i++)
+        //INSERCIÓN
+        for(int i = 0; i < 10; i++)
             l.insertN(i);
-        l.cabeza.getRigth().setUp(new NodoS(4));
-        System.out.println(l.cabeza.getRigth().getUp().getElem());
-        System.out.println("\nNúmero de elementos: " + l.cont + "\nAltura: " + l.altura + "\nImpesión:\n"+ l.imp());
+        System.out.println("Número de elementos: " + l.cont + "\nAltura: " + l.altura + "\nImpresión:\n"+ l.imp());
+        
+        //BORRADO
+        l.borra(8);
+        l.borra(1);
+        
+        System.out.println("Número de elementos: " + l.cont + "\nAltura: " + l.altura + "\nImpresión:\n"+ l.imp());
+        
     }
 }
